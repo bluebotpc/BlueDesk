271900764ba446326873dd6fd3a916d98c9c14a8
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2025-02-17 16:36:49 -0600

    Update local_webhook_handler.py
    debug note was inaccurate with new function name.

--- local_webhook_handler.py
+++ local_webhook_handler.py
@@ -68,7 +68,7 @@ def send_TktUpdate_discord_notification(ticket_number, ticket_status):
         response.raise_for_status()  # Raise exception for HTTP errors
         
         if response.status_code == 204:
-            print(f"INFO - WEBHOOK HANDLER - Ticket {ticket_number} has been closed notification sent to Discord.")
+            print(f"INFO - WEBHOOK HANDLER - Ticket {ticket_number} status change notification sent to Discord.")
         else:
             print(f"WARNING - WEBHOOK HANDLER - Unexpected response code: {response.status_code}")
 
